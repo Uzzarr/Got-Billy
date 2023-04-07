a961cd529c2842e66fa82dd0a6202604bf477c14
Uzzar <46665105+Uzzarr@users.noreply.github.com>
2023-04-06 19:23:20 -0500

    publishfolder

--- Program.cs
+++ Program.cs
@@ -5,6 +5,38 @@ using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add Azure App Configuration to the container.
+var azAppConfigConnection = builder.Configuration["AppConfig_connectionstrname"];
+if (!string.IsNullOrEmpty(azAppConfigConnection))
+{
+    // Use the connection string if it is available.
+    builder.Configuration.AddAzureAppConfiguration(options =>
+    {
+        options.Connect(azAppConfigConnection)
+        .ConfigureRefresh(refresh =>
+        {
+            // All configuration values will be refreshed if the sentinel key changes.
+            refresh.Register("TestApp:Settings:Sentinel", refreshAll: true);
+        });
+    });
+}
+else if (Uri.TryCreate(builder.Configuration["Endpoints:AppConfig_connectionstrname"], UriKind.Absolute, out var endpoint))
+{
+    // Use Azure Active Directory authentication.
+    // The identity of this app should be assigned 'App Configuration Data Reader' or 'App Configuration Data Owner' role in App Configuration.
+    // For more information, please visit https://aka.ms/vs/azure-app-configuration/concept-enable-rbac
+    builder.Configuration.AddAzureAppConfiguration(options =>
+    {
+        options.Connect(endpoint, new DefaultAzureCredential())
+        .ConfigureRefresh(refresh =>
+        {
+            // All configuration values will be refreshed if the sentinel key changes.
+            refresh.Register("TestApp:Settings:Sentinel", refreshAll: true);
+        });
+    });
+}
+builder.Services.AddAzureAppConfiguration();
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
@@ -23,6 +55,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+app.UseAzureAppConfiguration();
 
 app.UseRouting();
 
